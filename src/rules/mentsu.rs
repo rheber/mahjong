9c62cea3e717e85acccf79fc3155ee092e816129
rheber <rheber@rediffmail.com>
2023-07-09 15:25:19 +1000

    shuntsu

--- src/rules/mentsu.rs
+++ src/rules/mentsu.rs
@@ -5,7 +5,7 @@
 use crate::*;
 
 /**
- * Whether a list of tiles is a pair.
+ * Whether a collection of tiles is a pair.
  */
 pub fn is_jantou(tiles: &[Pai]) -> bool {
     if tiles.len() != 2 {
@@ -17,7 +17,7 @@ pub fn is_jantou(tiles: &[Pai]) -> bool {
 }
 
 /**
- * Whether a list of tiles is a four-of-a-kind.
+ * Whether a collection of tiles is a four-of-a-kind.
  */
 pub fn is_kantsu(tiles: &[Pai]) -> bool {
     if tiles.len() != 4 {
@@ -31,7 +31,7 @@ pub fn is_kantsu(tiles: &[Pai]) -> bool {
 }
 
 /**
- * Whether a list of tiles is a three-of-a-kind.
+ * Whether a collection of tiles is a three-of-a-kind.
  */
 pub fn is_koutsu(tiles: &[Pai]) -> bool {
     if tiles.len() != 3 {
@@ -41,6 +41,50 @@ pub fn is_koutsu(tiles: &[Pai]) -> bool {
     let second = &tiles[1];
     let third = &tiles[2];
     return first == second && first == third;
+}
+
+/**
+ * Whether a collection of tiles is a run of three.
+ */
+pub fn is_shuntsu(tiles: &[Pai]) -> bool {
+    if tiles.len() != 3 {
+        return false;
+    }
+    let first = &tiles[0];
+    let second = &tiles[1];
+    let third = &tiles[2];
+
+    match first {
+        Pai::Jihai(_) => return false,
+        Pai::Suupai(tile1) => {
+            match second {
+                Pai::Jihai(_) => return false,
+                Pai::Suupai(tile2) => {
+                    match third {
+                        Pai::Jihai(_) => return false,
+                        Pai::Suupai(tile3) => {
+                            if tile1.shoku != tile2.shoku ||
+                                tile1.shoku != tile3.shoku {
+                                return false;
+                            }
+                            let mut ranks = [tile1.rank, tile2.rank, tile3.rank];
+                            ranks.sort();
+                            return ranks[0] + 1 == ranks[1] && ranks[1] + 1 == ranks[2];
+                        },
+                    }
+                },
+            }
+        },
+    }
+}
+
+/**
+ * Whether a collection of tiles is a complete hand.
+ */
+pub fn is_complete_hand(tiles: &[Pai]) -> bool {
+    // TODO: seven pairs and thirteen orphans
+
+    return false;
 }
 
 #[cfg(test)]
@@ -110,6 +154,17 @@ mod tests {
 
     #[test]
     fn four_five_of_bamboos_are_a_quad() {
+        let tile1 = Pai::Suupai(Suupai { shoku: Shoku::Pinzu, rank: 3, akadora: false });
+        let tile2 = Pai::Suupai(Suupai { shoku: Shoku::Pinzu, rank: 1, akadora: false });
+        let tile3 = Pai::Suupai(Suupai { shoku: Shoku::Pinzu, rank: 2, akadora: false });
+        assert_eq!(
+            is_shuntsu(&[tile1, tile2, tile3]),
+            true
+        );
+    }
+
+    #[test]
+    fn one_two_three_of_dots_is_a_run() {
         let tile1 = Pai::Suupai(Suupai { shoku: Shoku::Souzu, rank: 5, akadora: false });
         let tile2 = Pai::Suupai(Suupai { shoku: Shoku::Souzu, rank: 5, akadora: false });
         let tile3 = Pai::Suupai(Suupai { shoku: Shoku::Souzu, rank: 5, akadora: false });
